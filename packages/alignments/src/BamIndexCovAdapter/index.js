016e594ef3a424275c061a759c75b6ebb6002a63
Colin <colin.diesh@gmail.com>
2019-06-26 18:42:45 -0400

    Add averageFeatureLength

--- packages/alignments/src/BamIndexCovAdapter/index.js
+++ packages/alignments/src/BamIndexCovAdapter/index.js
@@ -8,12 +8,26 @@ import {
 
 export const configSchema = ConfigurationSchema(
   'BamIndexCovAdapter',
-  {},
-  { baseConfiguration: bamAdapterConfigSchema,explicitlyTyped: true},
+  {
+    averageFeatureLength: {
+      type: 'number',
+      defaultValue: 150,
+      description:
+        'Index coverage produces number of features overlapping a region. If you want per-base feature depth, this can correspond to #features * #avg_len / 16kb',
+    },
+  },
+  { baseConfiguration: bamAdapterConfigSchema, explicitlyTyped: true },
 )
 
 export class AdapterClass extends BamAdapter {
   static capabilities = ['getFeatures', 'getRefNames', 'getGlobalStats']
+
+  private averageFeatureLength = 150
+
+  constructor(opts) {
+    super(opts)
+    this.averageFeatureLength = opts.averageFeatureLength
+  }
 
   async getGlobalStats() {
     return { scoreMax: 5000, scoreMin: 0 }
@@ -27,13 +41,15 @@ export class AdapterClass extends BamAdapter {
    * @param {AbortSignal} [signal] optional signalling object for aborting the fetch
    * @returns {Observable[Feature]} Observable of Feature objects in the region
    */
-  getFeatures({ refName, start, end }, signal) {
+  getFeatures({ refName, start, end }, { signal }) {
     return ObservableCreate(async observer => {
       await this.setup()
       const records = await this.bam.indexCov(refName, start, end, {
         signal,
       })
+      console.log(this.averageFeatureLength)
       records.forEach(record => {
+        record.score = record.score * this.averageFeatureLength / 16384
         observer.next(new SimpleFeature({ id: record.start + 1, data: record }))
       })
       observer.complete()
