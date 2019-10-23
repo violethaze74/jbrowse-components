e014a002cf41d31efbefd16c7fb104a60aa46183
Colin <colin.diesh@gmail.com>
2019-10-22 21:46:37 -0400

    Make global typography smaller

--- packages/jbrowse-web/src/ui/theme.js
+++ packages/jbrowse-web/src/ui/theme.js
@@ -1,6 +1,9 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
 export default createMuiTheme({
+  typography: {
+    fontSize: 12,
+  },
   palette: {
     // type: 'dark',
     primary: { main: '#396494' },
